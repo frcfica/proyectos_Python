bf622c97b7e0993392239b02aa2e50f2f4ce4b17
Flavio <flavior.castro.fica@gmail.com>
2026-04-12 18:22:46 -0400

    orden

--- correos.instirucionales.py
+++ correos.instirucionales.py
@@ -1,15 +1,11 @@
 dominio = "@alumnos.santotomas.cl"
 print("--|Bienvenido al generador de correos|--")
-
 nombre = input("Ingrese su nombre: ").lower().strip()
 apellido = input("Ingrese su apellido: ").lower().strip()
-
 while True:
     año = input("Ingrese solo los últimos 2 dígitos de su año de nacimiento (Ej: 98): ")
-    
     if len(año) == 2 and año.isdigit():
         break
     else:
         print("Error: Debe ingresar exactamente 2 números.")
-
 print(f"\nSu correo institucional es: {nombre}.{apellido}{año}{dominio}")
