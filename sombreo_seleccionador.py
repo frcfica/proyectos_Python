4a474702c64a867f02f6dcdbba6ab3a6b7ec0cbe
Flavio <flavior.castro.fica@gmail.com>
2026-04-25 14:12:46 -0400

    sombrero seleccionador

---
+++ sombreo_seleccionador.py
@@ -0,0 +1,8 @@
+def sombrero_seleccionador():
+    puntajes = {
+        "Gryffindor": 0,
+        "Hufflepuff": 0,
+        "Ravenclaw": 0,
+        "Slytherin": 0
+    }
+print("==|Bienvenido al sombrero seleccionador|==")
